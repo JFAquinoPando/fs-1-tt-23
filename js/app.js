239fb9cf362ef3c262c60e74a2a99a47bd3c8864
JFAquinoPando <pandolcik@gmail.com>
2025-09-17 20:42:00 -0300

    Agregamos la delegación de eventos

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const btnGuardar = document.querySelector("[data-name='btn-guardar']");
 const textoTarea = document.querySelector("input[name='nueva-tarea']");
 const listadoTareas = document.querySelector("ul");
 const btnLimpiar = document.querySelector("#btn-limpiar")
-const btnsEliminar = document.querySelectorAll(".eliminar")
+let btnsEliminar = []
 
 btnGuardar.addEventListener("click", function(evento){
     console.log(textoTarea.value);
@@ -20,13 +20,31 @@ btnLimpiar.addEventListener("click", function(evento){
 })
 
 
-console.log(btnsEliminar);
+listadoTareas.addEventListener("click", function(evento){
+    console.log("Evento: ", evento.target);
 
-btnsEliminar.forEach(
-    function(elemento){
-        elemento.addEventListener("click", function (evento) {
-            evento.target.closest("li").outerHTML = ""
-        })
+    if (evento.target.tagName == "BUTTON") {
+        evento.target.closest("li").remove()
     }
-)
+
+    if (evento.target.tagName == "SPAN") {
+        alert(evento.target.textContent)
+    }
+    
+})
+
+/* setInterval(
+    function () {
+        btnsEliminar =  document.querySelectorAll(".eliminar")
+
+        btnsEliminar.forEach(
+            function(elemento){
+                elemento.addEventListener("click", function (evento) {
+                    evento.target.closest("li").outerHTML = ""
+                })
+            }
+        )
+    }
+    , 3000) */
+
 
